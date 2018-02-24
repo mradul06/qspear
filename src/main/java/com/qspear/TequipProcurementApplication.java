bf15e961db1af35f7910541811fec181e3d5eee9
Mradul91 <91mradul@gmail.com>
2018-02-24 14:10:19 +0530

    Creating the project structure

---
+++ src/main/java/com/qspear/TequipProcurementApplication.java
@@ -0,0 +1,12 @@
+package com.qspear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TequipProcurementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TequipProcurementApplication.class, args);
+	}
+}
---
+++ src/test/java/com/qspear/TequipProcurementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qspear;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TequipProcurementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
